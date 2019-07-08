8f632b68f8033a0b026d10ee81ecf1a3bd4feada
Petar Scepanovic <hiborija@gmail.com>
2019-07-08 20:30:49 +0200

    finally finished the project, the basic requirments. I have not tried stretch becuase I struggled so much today

--- index.js
+++ index.js
@@ -1,1 +1,85 @@
 // implement your API here
+
+const express = require('express');
+const db = require('./data/db.js');
+
+const server = express();
+server.use(express.json());
+
+server.get('/api/users', (req, res) => {
+  db.find()
+    .then(users => {
+      res.status(200).json(users);
+      console.log(users);
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: 'The users information could not be retrieved.'
+      });
+    });
+});
+
+server.post('/api/users', (req, res) => {
+  const { name, bio } = req.body;
+  const newUser = {
+    name,
+    bio
+  };
+
+  db.insert(newUser)
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ error: 'Please provide name and bio for the user.' });
+    });
+});
+
+server.delete('/api/users/:id', (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  db.remove(id)
+    .then(user => {
+      if (!user) {
+        return res
+          .status(404)
+          .json({ message: 'The user with the specified ID does not exist.' });
+      }
+      res.status(200).json({
+        message: 'user has been deleted successfully'
+      });
+    })
+    .catch(() =>
+      res.status(500).json({ error: 'The user could not be removed' })
+    );
+});
+
+server.put('/api/users/:id', (req, res) => {
+  const { id } = req.params;
+  const { name, bio } = req.body;
+  const newUser = {
+    name,
+    bio
+  };
+
+  db.update(id, newUser)
+    .then(data => {
+      if (!data) {
+        res
+          .status(404)
+          .json({ message: 'The user with the specified ID does not exist.' });
+      }
+      res.status(200).json(data);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ error: 'Please provide name and bio for the user.' });
+    });
+});
+
+server.listen(5000, () => {
+  console.log('\n*** Server Running on http://localhost:5000 ***\n');
+});
